c16112376ee170ad0274e8b8c700c3a31b9e1eb2
Dens-engAi <densdorismondpro@gmail.com>
2026-09-12 16:09:43 +0200

    doc : add comments in the perceptron's file

--- src/models/perceptron.js
+++ src/models/perceptron.js
@@ -1,4 +1,4 @@
-
+// the activation fonction for the perceptron
 function step(z){
     if(z >= 0) {
         return 1;
@@ -7,20 +7,24 @@ function step(z){
     }
 }
 
-
+// The forward function of the perceptron 
 function perceptronForward(X, Y, w1, w2, b){
    let z = X * w1 + Y * w2 + b;
-    return setp(z) 
+    return step(z) 
 }
 
 
 function trainPerceptron(data, epochs = 100, learning_rate = 0.1) {
+    
+    // initializing the weights
     let w1 = Math.random() * 2 - 1;
     let w2 = Math.random() * 2 - 1; 
     let b = Math.random() * 2 - 1; 
 
+    // where we are collecting the epochs for the graph
     let history = []; 
 
+    // The for loop where the perceptron is beeing trained 
     for (let epoch = 0; epoch < epochs; epoch++) {
         let totalError = 0; 
 
@@ -29,14 +33,17 @@ function trainPerceptron(data, epochs = 100, learning_rate = 0.1) {
             const prediction = perceptronForward(X, Y, w1, w2, b);
             const error = target - prediction; 
 
+            // updating the weights
             w1 = w1 + learningRate * error * X; 
             w2 = w2 + learning_rate * error * Y; 
             b = b + learning_rate * error; 
 
             totalError += Math.abs(error); 
         }
+        // collecting epoch ans totalError 
         history.push({ epoch, totalError}); 
     }
+    
     return { w1, w2, b, history}; 
 } 
 
